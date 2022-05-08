88bb8d03aea944661eee3b92fc1ae959fc982b97
Jimmy Murillo <salchichongallo1997@gmail.com>
2022-05-07 23:16:19 -0500

    feat(items): init module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ItemsModule } from './items/items.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ItemsModule,
   ],
   controllers: [],
   providers: [],
